ae92271850742f055994f6d7201dcaaafc8f306d
Tobias Gagnon <90637945+TheShadowRaptor@users.noreply.github.com>
2022-11-07 14:53:39 -0400

    [FINISHED] PrimeNumbers

--- PrimeNumbers2/Program.cs
+++ PrimeNumbers2/Program.cs
@@ -8,8 +8,30 @@ namespace PrimeNumbers2
 {
     internal class Program
     {
+        static ulong maxNumber = 1000000000000;
         static void Main(string[] args)
         {
+            for (ulong i = 0; i < maxNumber; i++)
+            {
+                if (IsPrime(i) == true)
+                {
+                    Console.WriteLine(i + " = " + IsPrime(i) + ", ");
+                }
+            }
+            Console.ReadKey(true);
+        }
+
+        static bool IsPrime(ulong num)
+        {
+            if (num < 2) return false;
+            for (ulong i = 2; i < Math.Sqrt(num); i++)// i = 1... num -1 
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
